3635ec17c34a178d8dc54a3794c2b6a867d51e0d
isumizumi <isumi.karina72@gmail.com>
2017-01-31 19:18:29 +0700

    Finish

--- binary.js
+++ binary.js
@@ -28,13 +28,30 @@ var test_array_ganjil = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 function binary_search (search, array) {
   // Your code here
+  var left = 0;
+  var right = array.length-1;
+
+  while(left<=right){
+    var mid = Math.floor((left+right)/2);
+
+    if(array[mid]<search){
+      left = mid+1;
+    }
+    else if(array[mid]>search){
+      right = mid-1
+    }
+    else{
+      return mid;
+    }
+  }
+  return -1;
 }
 
 // Driver code
-console.log(binary_search(5, test_array_a))
-console.log(binary_search(10, test_array_a))
-console.log(binary_search(2, test_array_a))
+console.log(binary_search(5, test_array_genap))
+console.log(binary_search(10, test_array_genap))
+console.log(binary_search(2, test_array_genap))
 
-console.log(binary_search(6, test_array_b))
-console.log(binary_search(11, test_array_b))
-console.log(binary_search(2, test_array_b))
+console.log(binary_search(6, test_array_ganjil))
+console.log(binary_search(11, test_array_ganjil))
+console.log(binary_search(2, test_array_ganjil))
